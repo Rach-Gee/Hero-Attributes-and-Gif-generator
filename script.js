deacaf719514d9e2879b553657cae76673248b65
Ice Tea <shauntobin88@gmail.com>
2021-07-12 21:26:37 +0800

    appending stats

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var heroG = document.querySelector('#hero-gif');
 
+var resultContentEl = document.getElementById('text-content');
+
 fetch('https://api.giphy.com/v1/gifs/search?api_key=J93RdrgwiH30xTwsClipfYXrfhml0fkg&q=hulk', {
 })
   .then(function (response) {
@@ -23,19 +25,61 @@ fetch('https://superheroapi.com/api.php/10160024845509883/332/powerstats', {
     return response.json();
   })
   .then(function (data) {
-    console.log(data['power']);
-   
+    console.log(data);
+    printResults(data);
+    
+
+
+    
   }); 
 
  
 
-/*fetch('https://superheroapi.com/api/10160024845509883', {
-})
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
-*/
+
+
+//rendering results for current days on page 
+function printResults(resultObj) {
+  console.log(resultObj);
+  resultContentEl.innerHTML = ''
+
+  //creating element
+
+  var resultCard = document.createElement('div');
+  //adding classes
+
+  
+  var resultBody = document.createElement('div');
+ 
+  resultCard.append(resultBody);
+  
+  var nameEl = document.createElement('h2');
+  nameEl.textContent = resultObj.name
+
+  var intelligenceEl = document.createElement('p');
+  intelligenceEl.innerHTML = "Intelligence: " + resultObj.intelligence
+
+  var strengthEl = document.createElement('p');
+  strengthEl.innerHTML = "Strength: " + resultObj.strength
+
+  var speedEl = document.createElement('p');
+  speedEl.innerHTML = "Speed: " + resultObj.speed
+
+  var durabilityEl = document.createElement('p');
+  durabilityEl.innerHTML = "Durability: " + resultObj.durability
+
+  var powerEl = document.createElement('p');
+  powerEl.innerHTML = "Power: " + resultObj.power 
+
+  var combatEl = document.createElement('p');
+    combatEl.textContent = "Combat: " + resultObj.combat;
+  
+  
+  //appending data to the page
+  resultBody.append(nameEl, intelligenceEl, strengthEl, speedEl, durabilityEl, powerEl, combatEl);
+  resultContentEl.append(resultCard);
+}
+ 
+
+
+
   
